dfe232af77271f8c9e7819f2cd488bd642fa4c42
Reese Lee <reese.lee.cy@gmail.com>
2019-06-21 12:02:37 -0700

    add new files from latest install

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+      apiKey: "AIzaSyDXlEavR5H2mOwGbhToZcIRKhzeFoqMihY",
+      authDomain: "ohi-clone-66f08.firebaseapp.com",
+      databaseURL: "https://ohi-clone-66f08.firebaseio.com/",
+      projectId: "ohi-clone-66f08",
+      storageBucket: "gs://ohi-clone-66f08.appspot.com",
+      messagingSenderId: "361127401187"
+    }
 };
