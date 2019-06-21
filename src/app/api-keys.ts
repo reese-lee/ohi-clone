c114db6b1c961bae2f4e4ea2224f3789e82faa66
Reese Lee <reese.lee.cy@gmail.com>
2019-06-21 10:17:53 -0700

    add modified files to push

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export const masterFirebaseConfig = {
+    apiKey: "AIzaSyDXlEavR5H2mOwGbhToZcIRKhzeFoqMihY",
+    authDomain: "ohi-clone-66f08.firebaseapp.com",
+    databaseURL: "https://ohi-clone-66f08.firebaseio.com/",
+    storageBucket: "gs://ohi-clone-66f08.appspot.com",
+    messagingSenderId: "361127401187"
+  };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+// import { routing } from './app.routing';
 import { ParallaxBannerComponent } from './parallax-banner/parallax-banner.component';
 import { FooterComponent } from './footer/footer.component';
 import { NewsComponent } from './news/news.component';
@@ -26,7 +28,12 @@ export const firebaseConfig = {
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    FormsModule,
+    HttpModule,
+    // routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
