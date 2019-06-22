958986c1f0cc9ee1ba28e34c86d13582eb02927b
Reese Lee <reese.lee.cy@gmail.com>
2019-06-21 17:00:57 -0700

    add import statements for Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,37 @@ import { AppComponent } from './app.component';
 import { ParallaxBannerComponent } from './parallax-banner/parallax-banner.component';
 import { FooterComponent } from './footer/footer.component';
 import { NewsComponent } from './news/news.component';
+// import { Component } from '@angular/core';
+// import { AngularFirestore } from '@angular/fire/firestore';
+// import { Observable } from 'rxjs';
+//
+//
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//   <ul>
+//     <li *ngFor="let item of items | async">
+//       {{ item.name }}
+//     </li>
+//   </ul>
+//   `
+// })
+// export class MyApp {
+//   items: Observable<any[]>;
+//   constructor(db: AngularFirestore) {
+//     this.items = db.collection('items').valueChanges();
+//   }
+// }
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
@@ -20,7 +51,9 @@ import { NewsComponent } from './news/news.component';
     BrowserModule,
     MDBBootstrapModule.forRoot(),
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
